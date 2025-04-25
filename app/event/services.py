da79c2894f32ae3c52185ce310029f9ec3aec1fe
joekariuki3 <joekariuki3@gmail.com>
2025-04-25 17:38:05 +0300

    feat(event services): enhance event retrieval functions and fix parameter names in create_event

--- app/event/services.py
+++ app/event/services.py
@@ -1,6 +1,7 @@
 from app.extensions import db
 from app.models.event import Event
 from app.services.service_base import service_response
+from datetime import datetime
 from typing import Tuple, Union
 
 def create_event(event_date: str, event_name: str, family_id: int, event_location: Union[str, None], event_description: Union[str, None]) -> Tuple[dict, int]:
@@ -21,8 +22,8 @@ def create_event(event_date: str, event_name: str, family_id: int, event_locatio
         event = Event(
         event_date=event_date,
         event_name=event_name,
-        location=event_location,
-        description=event_description,
+        event_location=event_location,
+        event_description=event_description,
         family_id=family_id)
 
         db.session.add(event)
@@ -33,3 +34,37 @@ def create_event(event_date: str, event_name: str, family_id: int, event_locatio
         print(f"Error creating event: {str(e)}")
         db.session.rollback()
         return service_response(500, "Error creating event", "danger", None)
+
+def get_upcoming_events(family_id: int):
+    """
+    Retrieves upcoming events for a specific family.
+
+    Args:
+        family_id (int): The ID of the family.
+
+    Returns:
+        Tuple[dict, int]: A service response containing the list of upcoming events and a status code.
+    """
+    try:
+        currentTime = datetime.now()
+        events = db.session.query(Event).order_by(Event.event_date.asc()).filter_by(family_id=family_id).filter(Event.event_date>=currentTime ).all()
+        return service_response(200, "Upcoming events retrieved successfully", "success", events)
+    except Exception as e:
+        return service_response(500, "Error retrieving upcoming events", "danger", None)
+
+def get_past_events(family_id: int):
+    """
+    Retrieves past events for a specific family.
+
+    Args:
+        family_id (int): The ID of the family.
+
+    Returns:
+        Tuple[dict, int]: A service response containing the list of past events and a status code.
+    """
+    try:
+        currentTime = datetime.now()
+        events = db.session.query(Event).order_by(Event.event_date.asc()).filter_by(family_id=family_id).filter(Event.event_date<=currentTime ).all()
+        return service_response(200, "Past events retrieved successfully", "success", events)
+    except Exception as e:
+        return service_response(500, "Error retrieving past events", "danger", None)
