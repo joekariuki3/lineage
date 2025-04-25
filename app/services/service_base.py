72df1c54e693f8a647081098fb80411427b35ede
joekariuki3 <joekariuki3@gmail.com>
2025-04-25 18:16:32 +0300

    feat(service base): update service_response function to accept Union type for data parameter

--- app/services/service_base.py
+++ app/services/service_base.py
@@ -1,5 +1,5 @@
-from typing import Tuple, Optional
-def service_response(status_code: int, message: str, category: str, data: Optional[dict]) -> Tuple[dict, int]:
+from typing import Tuple, Union, List
+def service_response(status_code: int, message: str, category: str, data: Union[dict, List, None]) -> Tuple[dict, int]:
     """
     Generates a standard response for a service in the application.
 
@@ -7,7 +7,7 @@ def service_response(status_code: int, message: str, category: str, data: Option
         status_code (int): The HTTP status code to return.
         message (str): A message to the user.
         category (str): A category to group the response under. Should be 'success', 'error', or 'warning'.
-        data (Optional[dict]): Additional data to include in the response.
+        data (Union[dict, List, None]): Additional data to include in the response.
 
     Returns:
         Tuple[dict, int]: A tuple containing a dictionary with the message, category, and data, and the status code.
