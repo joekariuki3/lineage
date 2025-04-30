b088f9da4b242f7d7fd0ed60dfd265dabc81086e
joekariuki3 <joekariuki3@gmail.com>
2025-04-30 19:58:18 +0300

    feat(event routes): enhance delete and edit event functionality with session-based family ID retrieval and improved error handling

--- app/event/routes.py
+++ app/event/routes.py
@@ -1,11 +1,11 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, session
 from flask_login import current_user, login_required
 from . import bp
 from datetime import datetime
 from app.models.event import Event
 from .forms import AddEventForm
 from app.extensions import db
-from .services import create_event, get_upcoming_events, get_past_events, delete_an_event
+from .services import create_event, get_upcoming_events, get_past_events, delete_an_event, get_event, event_belongs_to_current_user, update_event
 
 @bp.route('/event/', methods=['POST', 'GET'])
 @login_required
@@ -68,22 +68,42 @@ def delete_event(event_id):
     else:
         flash(message, category)
 
-    return redirect(url_for('event.get_events', family_id=current_user.current_family_id))
+    return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
 
 @bp.route('/edit/event/<event_id>', methods=['GET', 'POST'])
 @login_required
 def edit_event(event_id):
-    event = Event.query.get_or_404(event_id)
+    data, status = get_event(event_id)
+    event, message, category = data.get('data'), data.get('message'), data.get('category')
+    if status != 200:
+        flash(message, category)
+        return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
+    elif not event:
+        flash(message, category)
+        return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
+
+    if not event_belongs_to_current_user(event, current_user):
+        flash('You are not authorized to edit this event', 'danger')
+        return redirect(url_for('event.get_events', family_id=session.get("current_family_id")))
+
     form = AddEventForm()
-    user_ids = [family.family_id for family in current_user.families]
-    if form.validate_on_submit() and event.family_id in user_ids:
-        event.event_date = form.date.data
-        event.event_name = form.name.data
-        event.location = form.location.data
-        event.description = form.description.data
-        db.session.commit()
-        flash(f'{event.event_name} details Updated', 'success')
+
+    if form.validate_on_submit():
+        data, status = update_event(
+            event = event,
+            event_date=form.date.data,
+            event_name=form.name.data,
+            event_location=form.location.data,
+            event_description=form.description.data
+        )
+        event, message, category = data.get('data'), data.get('message'), data.get('category')
+
+        if status != 200:
+            flash(message, category)
+            return render_template('edit_event.html', title='Update Event details', event=event, form=form)
+        flash(message, category)
         return redirect(url_for('event.get_events', family_id=event.family_id))
+
     form.description.data = event.description
     return render_template('edit_event.html', title='Update Event details', event=event, form=form)
 
